dbe04db537218e0f2f4f9beb5d0a63098243cdb4
Elijah-J <elijukanovich@gmail.com>
2021-03-22 20:56:29 -0700

    Allow user to select city to display weather data for

--- src/components/WeatherSearchBar.jsx
+++ src/components/WeatherSearchBar.jsx
@@ -4,12 +4,24 @@ import InputAdornment from "@material-ui/core/InputAdornment";
 import TextField from "@material-ui/core/TextField";
 import SearchIcon from "@material-ui/icons/Search";
 
-const WeatherSearchBar = () => {
+const WeatherSearchBar = (props) => {
+  const { onSubmit } = props;
+
+  const prepareInput = (input) => {
+    input = input.trim();
+    input = input.replace(/\s+/g, " ");
+    input = input.replace(/\s/g, "+");
+    return input;
+  };
+
   return (
     <div className="WeatherSearchBar">
       <TextField
         label="Search a City"
         size="small"
+        onKeyUp={(e) => {
+          if (e.key === "Enter") onSubmit(prepareInput(e.target.value));
+        }}
         variant="outlined"
         InputProps={{
           startAdornment: (
